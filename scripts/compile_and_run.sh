903832b574cf6a3f812bd19443d35459dba08583
Aayush Shukla <oreus.aks@gmail.com>
2026-02-28 23:15:23 +0530

    feat: add initial Turf compiler source code, local build script, and github actions release workflow.

--- scripts/compile_and_run.sh
+++ scripts/compile_and_run.sh
@@ -6,7 +6,7 @@ GREEN="\033[0;32m"
 RED="\033[0;31m"
 RESET="\033[0m"
 
-TOTAL_STEPS=5
+TOTAL_STEPS=3
 
 if [ $# -eq 0 ]; then
     echo "Usage: $0 <input.tr>"
@@ -17,9 +17,8 @@ INPUT_FILE=$1
 
 cleanup() {
     EXIT_CODE=$?
-    
-    echo -e "${GREEN}[5 / ${TOTAL_STEPS}]${RESET} ${BOLD}Cleaning up temporary files...${RESET}"
-    rm -f output.o
+
+    echo -e "${GREEN}[3 / ${TOTAL_STEPS}]${RESET} ${BOLD}Cleaning up temporary files...${RESET}"
     rm -f program
 
     if [ $EXIT_CODE -eq 0 ]; then
@@ -34,15 +33,9 @@ trap cleanup EXIT INT TERM
 COMPILER_PATH="$(dirname "$0")/../turf"
 
 echo -e "${GREEN}[1 / ${TOTAL_STEPS}]${RESET} ${BOLD}Compiling Turf source:${RESET} $INPUT_FILE"
-"$COMPILER_PATH" "$INPUT_FILE"
+"$COMPILER_PATH" "$INPUT_FILE" -o program
 
-echo -e "${GREEN}[2 / ${TOTAL_STEPS}]${RESET} ${BOLD}Generating object file (PIC mode)...${RESET}"
-llc -relocation-model=pic -filetype=obj output.ll -o output.o
-
-echo -e "${GREEN}[3 / ${TOTAL_STEPS}]${RESET} ${BOLD}Linking executable...${RESET}"
-clang output.o -o program -lm
-
-echo -e "${GREEN}[4 / ${TOTAL_STEPS}]${RESET} ${BOLD}Running program output:${RESET}"
+echo -e "${GREEN}[2 / ${TOTAL_STEPS}]${RESET} ${BOLD}Running program output:${RESET}"
 echo ""
 
 ./program
